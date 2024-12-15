06e51eccea330790431eea82551e5d29e83af55b
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2024-12-15 19:47:25 +0530

    Added Rotation to the Shirt and added Tabs for Customization.

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+/*import React from 'react';
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal , useGLTF, useTexture } from '@react-three/drei';
 import state from '../store';
-import { Group } from 'three';
-import * as THREE from 'three';
 
 const Shirt = () => {
 
@@ -50,4 +48,68 @@ const stateString = JSON.stringify(snap);
   )
 }
 
-export default Shirt
+export default Shirt*/
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import state from '../store';
+
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  // Rotate the shirt model dynamically
+  const shirtRef = React.useRef();
+
+  useFrame((_, delta) => {
+    // Update material color smoothly
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta);
+
+    // Apply rotation to the model
+    if (shirtRef.current) {
+      shirtRef.current.rotation.y += 0.01; // Rotate on the Y-axis
+      //shirtRef.current.rotation.x += 0.01; // Slight rotation on X-axis (optional)
+    }
+  });
+
+  const stateString = JSON.stringify(snap);
+
+  return (
+    <group ref={shirtRef} key={stateString}>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[-0.07, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.1}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
+    </group>
+  );
+};
+
+export default Shirt;
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -5,7 +5,7 @@ import { useSnapshot } from "valtio";
 import { AiPicker, ColorPicker, CustomButton, FilePicker, Tab} from "../components";
 import config from "../config/config";
 import state from "../store";
-import { download } from "../assets";
+import { download, logoShirt, stylishShirt } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
@@ -13,7 +13,28 @@ import { fadeAnimation, slideAnimation } from "../config/motion";
 
 function Customizer() {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generating, setGenerating] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  }) 
 
+  //show Tab Content based on the ActiveTab
+  const generateTabContent = () => {
+     switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker/>
+      case "aipicker":
+        return <AiPicker/>
+      case "filepicker":
+        return <FilePicker/>
+      default: 
+        return null;
+     }
+  }
   return (
     <AnimatePresence>
      {!snap.intro && (
@@ -29,9 +50,10 @@ function Customizer() {
             <Tab
               key={tab.name}
               tab={tab}
-              handleClick={()=> {}} 
+              handleClick={()=> setActiveEditorTab(tab.name)} 
             />
           ))}
+          {generateTabContent()}
          </div>
         </div>
        </motion.div>
